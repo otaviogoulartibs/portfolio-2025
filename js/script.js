b42bebd008fd9fd77bf4de2001f3308363518b89
Otavio Goularti <otaviopro26@gmail.com>
2025-07-23 13:19:22 -0300

    Melhora da performance das animações

--- js/script.js
+++ js/script.js
@@ -17,14 +17,10 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   
     function estaVisivel(elemento) {
-      const rect = elemento.getBoundingClientRect();
-      return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-      );
+    const rect = elemento.getBoundingClientRect();
+    return rect.top < window.innerHeight && rect.bottom >= 0;
     }
+
   
     window.addEventListener('scroll', verificarVisibilidade);
     verificarVisibilidade();
